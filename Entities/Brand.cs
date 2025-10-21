fe9ff5994c1006b0c5e298c488b225b03b43f045
MattiaGuazzini07 <145323347+MattiaGuazzini07@users.noreply.github.com>
2025-10-21 11:29:44 +0200

    Add entities for motorbike management including Brand, FuelType, GearBox, Motorbike, and Seller

---
+++ Entities/Brand.cs
@@ -0,0 +1,22 @@
+﻿namespace DashboardMoto.Entities;
+
+public enum Brand
+{
+    Aprilia,
+    BMW,
+    Ducati,
+    HarleyDavidson,
+    Honda,
+    Kawasaki,
+    KTM,
+    Suzuki,
+    Triumph,
+    Yamaha,
+    MotoGuzzi,
+    MvAgusta,
+    Benelli,
+    Piaggio,
+    Indian,
+    RoyalEnfield,
+    Other
+}
---
+++ Entities/Seller.cs
@@ -0,0 +1,8 @@
+﻿namespace DashboardMoto.Entities;
+
+public class Seller(int id, string name, string email)
+{
+    public int Id { get; set; } = id;
+    public string Name { get; set; } = name;
+    public string Email { get; set; } = email;
+}
