43fbe5411414f67182b10ddbce80a44d0377a1fc
Rohozin <rohozin.max@gmail.com>
2025-10-23 15:52:13 +0200

    security fix

--- Persistence/AppDbContext.cs
+++ Persistence/AppDbContext.cs
@@ -11,11 +11,6 @@ public class AppDbContext : DbContext
     public AppDbContext(DbContextOptions<AppDbContext> options)
         : base(options) { }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
-        optionsBuilder.UseNpgsql(
-            "Host=ep-muddy-dust-ab9o591a-pooler.eu-west-2.aws.neon.tech;Database=neondb;Username=neondb_owner;Password=npg_O3kHAnQoj5gC;"
-        );
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Motorbike>()
