d61633ae8bf03b3beb56a62fa592e7e5b90f54b2
Rohozin <rohozin.max@gmail.com>
2025-10-23 14:46:26 +0200

    feat: add migrations and created database

---
+++ Migrations/20251023102206_AddSellerWithForeignKey.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace DashboardMoto.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSellerWithForeignKey : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Sellers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sellers", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Motorbikes_SellerId",
+                table: "Motorbikes",
+                column: "SellerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Motorbikes_Sellers_SellerId",
+                table: "Motorbikes",
+                column: "SellerId",
+                principalTable: "Sellers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Motorbikes_Sellers_SellerId",
+                table: "Motorbikes");
+
+            migrationBuilder.DropTable(
+                name: "Sellers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Motorbikes_SellerId",
+                table: "Motorbikes");
+        }
+    }
+}
