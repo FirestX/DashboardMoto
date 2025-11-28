da249c037439c63b3837a6666f920e3b54c9f487
FirestX <rohozin.max@gmail.com>
2025-11-28 21:27:23 +0100

    feat: add enum dtos to keep parser simple

--- Entities/Dtos/MotorbikeDto.cs
+++ Entities/Dtos/MotorbikeDto.cs
@@ -9,8 +9,8 @@ public class MotorbikeDto
 	public double MileageKm { get; set; }
 	public string Location { get; set; }
 
-	public string? GearBoxType { get; set; }
-	public string BrandName { get; set; }
-	public string? FuelType { get; set; }
+	public GearboxDto? GearBoxType { get; set; }
+	public BrandDto BrandName { get; set; }
+	public FuelDto? FuelType { get; set; }
 	public string SellerName { get; set; }
 }
