31d6bc908e1c5d32743949bdd24efdfceacea65c
FirestX <rohozin.max@gmail.com>
2025-12-01 18:02:59 +0100

    fix: require fields

--- Entities/Dtos/MotorbikeDto.cs
+++ Entities/Dtos/MotorbikeDto.cs
@@ -3,14 +3,14 @@
 public class MotorbikeDto
 {
 	public double? HorsePower { get; set; }
-	public string Model { get; set; }
+	public required string Model { get; set; }
 	public DateTime PostDate { get; set; }
 	public double Price { get; set; }
 	public double MileageKm { get; set; }
-	public string Location { get; set; }
+	public required string Location { get; set; }
 
 	public GearboxDto? GearBoxType { get; set; }
 	public BrandDto BrandName { get; set; }
 	public FuelDto? FuelType { get; set; }
-	public string SellerName { get; set; }
+	public required string SellerName { get; set; }
 }
